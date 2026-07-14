4593e69fb0cae6f460336fba299e3e46b3b6996f
jatin170506 <jbhandari287@gmail.com>
2026-07-14 23:32:55 +0530

    Day 01: Solved CF 4A - Watermelon

---
+++ day 1/Codeforces Checking/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String cf = "codeforces";
+        int n = sc.nextInt();
+        
+        while(n-- > 0){
+            char c = sc.next().charAt(0);
+            if (cf.indexOf(c) != -1)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}

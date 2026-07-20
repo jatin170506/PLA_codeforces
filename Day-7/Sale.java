3b369d861a481c5136856faa68849b44948e1530
jatin170506 <jbhandari287@gmail.com>
2026-07-20 23:57:19 +0530

    Day-7, 30B- Sale

---
+++ Day-7/Sale.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+public class Sale{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+ 
+        int[] arr = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(arr);  
+ 
+        for (int i = 0; i < m; i++) {
+            if (arr[i] <= 0)
+                sum += Math.abs(arr[i]);
+        }
+ 
+        System.out.println(sum);
+    }
+}
